a14d603e9b1a3e5b00ca0b64cf9be24cf94f1f84
unknown <hrusheekeshsawarkar@gmail.com>
2024-10-13 22:33:34 +0530

    add mongo authentication in frontend

--- frontend/lib/mongodb.js
+++ frontend/lib/mongodb.js
@@ -2,8 +2,12 @@
 // 'use client'; // Use 'use client' directive for components with client-side interactivity
 
 import { MongoClient } from 'mongodb';
+var mongo_username =encodeURIComponent(process.env.MONGODB_USERNAME)
+var mongo_password =encodeURIComponent(process.env.MONGODB_PASSWORD)
+var mongo_url=`mongodb://${mongo_username}:${mongo_password}@34.171.126.76:27017/?authSource=admin`
 
-const client = new MongoClient(process.env.MONGODB_URI);
+console.log(mongo_url)
+const client = new MongoClient(mongo_url);
 const clientPromise = client.connect();
 
 export default clientPromise;
